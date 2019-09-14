b830761ae50a2c859d10ff4f32a1d23ccab0178e
nothing628 <yogyphang@gmail.com>
2019-09-14 22:33:33 +0700

    inital commit

---
+++ src/com/x5a/assetsystem/AssetSystem.java
@@ -0,0 +1,11 @@
+package com.x5a.assetsystem;
+
+public class AssetSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coba");
+		System.out.println("12");
+	}
+
+}
