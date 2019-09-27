7f18ffd2e6958030a37314dc53bf8701e1b36f7b
nothing628 <yogyphang@gmail.com>
2019-09-27 22:22:17 +0700

    include database script

--- src/com/x5a/assetsystem/database/DBConnection.java
+++ src/com/x5a/assetsystem/database/DBConnection.java
@@ -1,5 +1,7 @@
 package com.x5a.assetsystem.database;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,10 +11,17 @@ public class DBConnection {
 
     static {
         try {
-			Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:E:/test.db");
+            Class.forName("org.sqlite.JDBC");
+            String location = getDbFileLocation();
+            connection = DriverManager.getConnection("jdbc:sqlite:" + location);
 		} catch (SQLException | ClassNotFoundException e) {
 			System.out.print("Gagal konek" + e.getMessage());
 		}
+    }
+
+    public static String getDbFileLocation() {
+        Path path = Paths.get("asset_system.db");
+        // return "E:/test.db";
+        return path.toAbsolutePath().normalize().toString();
     }
 }
