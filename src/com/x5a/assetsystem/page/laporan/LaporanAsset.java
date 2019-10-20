2c296fa922538f8eac00aaba5bbacafed2bec825
nothing628 <yogyphang@gmail.com>
2019-10-20 13:16:58 +0700

    finish laporan asset

--- src/com/x5a/assetsystem/page/laporan/LaporanAsset.java
+++ src/com/x5a/assetsystem/page/laporan/LaporanAsset.java
@@ -1,6 +1,11 @@
 package com.x5a.assetsystem.page.laporan;
 
 import com.x5a.assetsystem.page.PageBase;
+import com.x5a.assetsystem.database.DBConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LaporanAsset extends PageBase {
 
@@ -17,8 +22,27 @@ public class LaporanAsset extends PageBase {
 
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		
+		try {
+			Statement stat = DBConnection.connection.createStatement();
+			ResultSet set = stat.executeQuery("SELECT * FROM asset "
+					+ "JOIN asset_detail ON asset.id = asset_detail.id_asset");
+			
+			printLine();
+			while (set.next()) {
+				String row_table = String.format(
+						"| %20s | %15s | %10s | %10s | %10s | %6s |",
+						set.getString("nama_asset"),
+						set.getString("kategori"),
+						set.getString("jenis"),
+						set.getString("lokasi"),
+						set.getString("pegawai"),
+						set.getString("kondisi"));
+
+				println(row_table);
+			}
+		} catch (SQLException e) {
+			println("Error: Tidak bisa menampilkan data");
+		}
 	}
 
 }
